66d0c7cce5d19b896a350b94eec6cfa3834d60d4
BrevanBender <bendercoding@gmail.com>
2022-02-28 14:34:26 -0800

    render function working for my projects

--- app.js
+++ app.js
@@ -3,49 +3,59 @@ const myProjects = [
     id: "proj1",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    buttons: ["Deployed site", "Source Code"],
     links: ["the url of app", "the github repo"]},
     
     { name: "place holder 2",
     id: "proj2",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    buttons: ["Deployed site", "Source Code"],
     links: ["the url of app", "the github repo"]},
     
     { name: "place holder 3",
     id: "proj3",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    buttons: ["Deployed site", "Source Code"],
     links: ["the url of app", "the github repo"]},
 
     { name: "place holder 4",
     id: "proj4",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    buttons: ["Deployed site", "Source Code"],
     links: ["the url of app", "the github repo"]},
     
     { name: "place holder 5",
     id: "proj5",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    buttons: ["Deployed site", "Source Code"],
     links: ["the url of app", "the github repo"]},
     
     { name: "place holder 6",
     id: "proj6",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    buttons: ["Deployed site", "Source Code"],
     links: ["the url of app", "the github repo"]}
 ]
 
 //loop and append onto container
 
 for(let project of myProjects){
-    let $projSect = $(`<div id="${project.id}"><div>`)
+    console.log(project.name)
+    let $projSect = $(`<div class="projSect"><div>`)
+    //create container
+    $projSect.append(`<h3>${project.name}<h3>`)
+    //add name to container
+    let $techHolder = $('<div class="techHolder"><div>')
+    for(let tech of project.technologies){
+        //loop and add string of array contents
+        console.log(tech)
+        $techHolder.append(`<span>${tech}, <span>`)
+    }
+    $projSect.append($techHolder)
+    $projSect.append(`<p id="${project.id}p" class="hidme">${project.about}<p>`)
+    let $buttonHolder = $('<div class="buttonHolder"><div>')
+    $buttonHolder.append($(`<a src="${project.links[1]}"><button class="linkButton">Link to Site</button></a>`))
+    $buttonHolder.append($(`<a src="${project.links[2]}"><button class="linkButton">Link to Repo</button></a>`))
+    $projSect.append($buttonHolder)
     $("#project-container").append($projSect)
 }
 
