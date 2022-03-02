33d73040702541a2fe2734cb77456b41c3cf0e14
BrevanBender <bendercoding@gmail.com>
2022-03-02 11:54:23 -0800

    every page is built out and responsive

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-function sendEmail(){
-    Email.send({
-        Host : "smtp.yourisp.com",
-        Username : "bendercoding@gmail.com",
-        Password : "Falcon2013",
-        To : 'bendercoding@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "Contact From Portfolio",
-        Body : `Name: ${document.getElementById("name")}
-        <br>email: ${document.getElementById("email")}
-        <br>Message: ${document.getElementById("message")}`
-    }).then(
-      message => alert(message)
-    );
-}
+$('#emailMe').on('click', ()=>{
+    console.log("clicked")
+    $('#modalContainer').css("display", "block")
+})
+$("#closeThis").on('click', ()=>{
+    location.reload()
+})
