edfec8ed57036cbf88fc40268182dff2db435a56
BrevanBender <bendercoding@gmail.com>
2022-04-14 10:47:35 -0700

    working through lunch

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ $("#closeThis").on('click', ()=>{
     location.reload()
 })
 const myProjects = [
-    { name: "place holder 1",
+    
+    { name: "GeoGram",
     id: "proj1",
     technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
     backgroundAddress: "https://images.unsplash.com/photo-1487058792275-0ad4aaf24ca7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNvZnR3YXJlJTIwZW5naW5lZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
     links: ["the url of app", "the github repo"]},
     
-    { name: "place holder 2",
+    { name: "Event Finder",
     id: "proj2",
-    technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
-    backgroundAddress: "https://images.unsplash.com/photo-1487058792275-0ad4aaf24ca7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNvZnR3YXJlJTIwZW5naW5lZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    links: ["the url of app", "the github repo"]},
+    technologies: ["CSS", "Express", "Mongoose", "MongoDB"],
+    backgroundAddress: "./assets/event.png",
+    about: "This fullstack express/mongoose app was a group effort with some of my great classmates. It has login logic complete with password encryption, user created and ticketmaster api events, a query search function, and all restful routes for two models.",
+    links: ["https://eventpalooza.herokuapp.com/events", "https://github.com/BrevanBender/event-finder"]},
     
-    { name: "place holder 3",
+    { name: "Pokemon Battling Simulator",
     id: "proj3",
-    technologies: ["CSS", "HTML", "JavaScript", "JQuery"],
-    backgroundAddress: "https://images.unsplash.com/photo-1487058792275-0ad4aaf24ca7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNvZnR3YXJlJTIwZW5naW5lZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    about: "I used these technologies to make this app that does this. With this project I really focused on using blank to blank",
-    links: ["the url of app", "the github repo"]},
-
+    technologies: ["CSS", "HTML", "JavaScript", "Cypress"],
+    backgroundAddress: "./assets/pokemonBattle.png",
+    about: "I collaborated with several others to complete this one week project. This app was produced using cypress and tdd to ensure everything was functioning as intended. While the other technologies used are pretty basic, it was challenging to manage the game logic. In the end we produced 8 layers of conditionals to allow over 50 unique outcomes per turn cycle and integrated a simplistic retro style to the app.",
+    links: ["https://brevanbender.github.io/pokemon-battling/", "https://github.com/BrevanBender/pokemon-battling/tree/brevanBranch"]},
   ]
 
 const tagLines = [
@@ -56,8 +56,8 @@ for(let project of myProjects){
     $modalProj.append(`<p id="${project.id}p" class="hideme">${project.about}<p>`)
     $projSect.append(`<p id="${project.id}p" class="hideme">${project.about}<p>`)
     let $buttonHolder = $('<div class="buttonHolder"id="hidemetoo"><div>')
-    $buttonHolder.append($(`<a src="${project.links[1]}"><button class="linkButton">Link to Site</button></a>`))
-    $buttonHolder.append($(`<a src="${project.links[2]}"><button class="linkButton">Link to Repo</button></a>`))
+    $buttonHolder.append($(`<a href="${project.links[0]}" target="_blank"><button class="linkButton">Link to Site</button></a>`))
+    $buttonHolder.append($(`<a href="${project.links[1]}" target="_blank"><button class="linkButton">Link to Repo</button></a>`))
     $projSect.append($buttonHolder)
     $modalProj.append($(`<button class="closeButton" id="${project.id}C">X<button>`))
     $projSect.css("background-image", `url(${project.backgroundAddress})`)
